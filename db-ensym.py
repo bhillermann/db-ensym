1f88e6134817e79981649d48d05df5b1361b34e6
Brendon Hillermann <bhillermann@gmail.com>
2023-09-04 17:03:12 +1000

    Refactored SQL statement to use IN statement. Declared constants at the top. Pushed DB config info to config file we import. So we are not pushing DB credentials to git.

--- db-ensym.py
+++ db-ensym.py
@@ -5,6 +5,21 @@ from datetime import date
 import pandas as pd
 import geopandas as gpd
 import argparse
+import json
+
+# Define constants
+db_config = ('/home/bhillermann/Documents/Development/Python/db-ensym/'
+             'db_config.json')
+
+# Define the Excel file to import
+evc_data = ('/home/bhillermann/Documents/GIS/Ensym/'
+            'EVC benchmark data - external use.xlsx')
+
+# define the Ensym constants
+project = "Python"
+collector = "Desktop"
+default_gain_score = 0.22
+default_habitat_score = 0.4
 
 # Call argparse and define the arguments
 parser = argparse.ArgumentParser(description=r'Process View PFIs to an Ensym'
@@ -22,61 +37,51 @@ parser.add_argument("-g", "--gainscore", type=float,
 args = parser.parse_args()
 
 
-# define the Ensym constants
-project = "Python"
-collector = "Desktop"
-# declare the variable for the SQL substitution
-sub_string = ''
+# Load the configuration from the JSON file
+with open(db_config, 'r') as config_file:
+    config = json.load(config_file)
 
+db_connection = config.get('db_connection')
 
 # Connect to your postgres DB
-url_object = URL.Create(
-    "postgresql+psycopg2",
-    username="gis",
-    password="#!gis^",
-    host="Brendon-Lenovo.local",
-    database="gisdb",
-    pool_size=20,
-    max_overflow=20
+url_object = URL.create(
+    db_connection['db_type'],
+    username=db_connection['username'],
+    password=db_connection['password'],
+    host=db_connection['host'],
+    database=db_connection['database']
 )
 
 engine = create_engine(url_object)
 
 # Define the SQL query
-sql_query = """
-select bio_clipped.* from
-(
-select clipped.evc, clipped.x_evcname, clipped.view_pfi, bio.bioregcode,
-   bio.bioregion, (st_dump(st_intersection(clipped.geom, bio.geom))).geom geom
-   from (
-       select evc.evc, evc.x_evcname, parcel.pfi as view_pfi,
-       (st_dump(st_intersection(st_buffer(parcel.geom,-6),
-           evc.geom))).geom geom
-       from parcel_view_gda94 parcel
-           inner join nv1750_evc_gda94 evc
-               on st_intersects(parcel.geom, evc.geom)
-                   where
-                       ==SUBS_TXT==
-    ) as clipped
-    inner join bioregions_gda94 bio
-    on st_intersects(clipped.geom, bio.geom)
-    where ST_Dimension(clipped.geom) = 2
-) as bio_clipped order by bioregcode
+sql_query = f"""
+SELECT bio_clipped.*
+FROM (
+    SELECT clipped.evc, clipped.x_evcname, clipped.view_pfi, bio.bioregcode,
+        bio.bioregion, (ST_Dump(ST_Intersection(clipped.geom, bio.geom))).geom\
+              geom
+    FROM (
+        SELECT evc.evc, evc.x_evcname, parcel.pfi AS view_pfi,
+            (ST_Dump(ST_Intersection(ST_Buffer(parcel.geom, -6),\
+                  evc.geom))).geom geom
+        FROM parcel_view_gda94 parcel
+        INNER JOIN nv1750_evc_gda94 evc
+        ON ST_Intersects(parcel.geom, evc.geom)
+        WHERE parcel.pfi IN ({', '.join([f"'{pfi}'" for pfi in args.view_pfi])})
+    ) AS clipped
+    INNER JOIN bioregions_gda94 bio
+    ON ST_Intersects(clipped.geom, bio.geom)
+    WHERE ST_Dimension(clipped.geom) = 2
+) AS bio_clipped
+ORDER BY bioregcode
 """
-
-# Process the arguments into the correct SQL `WHERE` statements
-if args.view_pfi[0]:  # First statment doesn't start with OR
-    sub_string += "parcel.pfi = \'" + str(args.view_pfi[0]) + "\'"
-if len(args.view_pfi) > 1:  # Needs an OR from second statement on
-    for i in args.view_pfi[1:]:
-        sub_string += "\n or \nparcel.pfi = \'" + str(i) + "\'\n"
 
 # define a counter for the different PFIs so we can assign proper zone IDs
 count = [0] * len(args.view_pfi)
 
 # Retrieve query resuls as GeoDataFrame substituting the sql query with
 # view_pfi arguments we collected
-sql_query = sql_query.replace("==SUBS_TXT==", sub_string)
 bioevc_gdf = gpd.GeoDataFrame.from_postgis(sql=text(sql_query),\
                                            con=engine.connect())
 if bioevc_gdf.empty:
@@ -88,9 +93,6 @@ bioevc_gdf = bioevc_gdf.set_crs('epsg:3111')
 
 
 # Import the EVC benchmark data with pandas
-# Define the Excel file to import
-evc_data = (r"~/Documents/GIS/Ensym/"
-            r"EVC benchmark data - external use.xlsx")
 # Open the Excel file. Quit if not FileNotFoundError
 try:
     evc_df = pd.read_excel(evc_data)
@@ -122,14 +124,14 @@ def find_hh_evc(row):
         bcs_value = bcs_value[0]
     # Set the correct Site ID if there are multiple parcels
     global count
-    if len(args.view_pfi) > 1:
-        for id in range(len(args.view_pfi)):
-            if int(args.view_pfi[id]) == int(row['view_pfi']):
-                si = (id + 1)
-                count[id] = count[id] + 1
-    else:
-        si = 1
-        count[si - 1] = count[si - 1] + 1
+
+    # Find the index of the current 'row['view_pfi']' within 'args.view_pfi'
+    si = args.view_pfi.index(int(row['view_pfi']))\
+          + 1 if len(args.view_pfi) > 1 else 1
+
+    # Update the count list
+    count[si - 1] += 1
+
     # Change the Zone ID from an integer to alpha
     if count[si - 1] <= 26:
         zi = chr(ord('@') + count[si - 1])
@@ -151,12 +153,12 @@ ensym_gdf['HH_VAC'] = "P"
 ensym_gdf[['HH_SI', 'HH_ZI', 'HH_EVC', 'BCS']] = \
     ensym_gdf.apply(lambda row: find_hh_evc(row), axis=1, result_type="expand")
 ensym_gdf['LT_CNT'] = 0
-ensym_gdf['HH_H_S'] = 0.4
+ensym_gdf['HH_H_S'] = default_habitat_score
 # Set the gainscore if specified, otherwise default to 0.22
 if args.gainscore:
     ensym_gdf['G_S'] = args.gainscore
 else:
-    ensym_gdf['G_S'] = 0.22
+    ensym_gdf['G_S'] = default_gain_score
 # Calculate the area in hecatres
 ensym_gdf['HH_A'] = ensym_gdf['geom'].area / 10000
 # Drop the extra columns
